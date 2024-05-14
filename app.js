44f00a9c8871a17f0eefd76c0d1fcb7b0084b41b
Matías Martearena <maticorvooficial@gmail.com>
2024-05-14 15:02:54 -0300

    3.3 Phonebook backend step 3

--- app.js
+++ app.js
@@ -21,6 +21,14 @@ app.get('/info', (req, res) => {
   `)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+
+  const person = persons.find(person => person.id === id)
+
+  person ? res.send(person) : res.status(404).end()
+})
+
 const PORT = process.env.PORT ?? 3001
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`)
